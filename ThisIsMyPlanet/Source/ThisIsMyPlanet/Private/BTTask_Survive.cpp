e416a4ea40586a97e391dcd7b214d9f17ca80d8d
Yanis SCHWARZ <y.schwarz@student.isartdigital.com>
2024-11-22 13:58:22 +0100

    Added: Animal Controller + Behaviour Tree

---
+++ ThisIsMyPlanet/Source/ThisIsMyPlanet/Private/BTTask_Survive.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_Survive.h"
+
+UBTTask_Survive::UBTTask_Survive()
+{
+    NodeName = "Survive";
+}
+
+EBTNodeResult::Type UBTTask_Survive::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    if (!OwnerComp.GetAIOwner())
+    {
+        return EBTNodeResult::Failed;
+    }
+
+    AAnimalController* Controller = Cast<AAnimalController>(OwnerComp.GetAIOwner());
+    Controller->Survive();
+
+    return EBTNodeResult::Succeeded;
+}
